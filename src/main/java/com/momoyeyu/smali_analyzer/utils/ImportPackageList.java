b8e882ecb880cf23fc8b54156c95b60b280696b6
Momoyeyu <momoyeyu@outlook.com>
2024-07-29 10:46:05 +0800

    fulfill import package utils

--- src/main/java/com/momoyeyu/smali_analyzer/utils/ImportPackageList.java
+++ src/main/java/com/momoyeyu/smali_analyzer/utils/ImportPackageList.java
@@ -1,19 +1,54 @@
 package com.momoyeyu.smali_analyzer.utils;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class ImportPackageList {
     private List<String> list;
-
     private static List<String> defaultImportPackageList = new LinkedList<>();
-
     static {
-        defaultImportPackageList.add("java.lang.*");
+        // 'java.lang' packages are imported by default.
+        defaultImportPackageList.add("java\\.lang(\\.\\S+)?");
+    }
+
+    /**
+     * Test
+     * @param args user input
+     */
+    public static void main(String[] args) {
+        ImportPackageList importPackageList = new ImportPackageList();
+        importPackageList.importPackage("java.lang.String"); // wouldn't be imported
+        importPackageList.importPackage("java.util.List"); // import
+        importPackageList.importPackage("java.util.ArrayList"); // import
+        importPackageList.importPackage("java.lang"); // wouldn't be imported
+        System.out.println(importPackageList);
     }
 
     public ImportPackageList() {
-        this.list = new LinkedList<String>();
+        this.list = new ArrayList<>();
+    }
+
+    public void importPackage(String packageName) {
+        packageName = packageName.strip();
+        for (String importPackage : defaultImportPackageList) {
+            if (packageName.matches(importPackage)) {
+                return;
+            }
+        }
+        if (packageName != null && !packageName.isEmpty()) {
+            this.list.add(packageName);
+        }
+    }
+
+    @Override
+    public String toString() {
+        list.sort(null);
+        StringBuilder sb = new StringBuilder();
+        for (String importPackage : this.list) {
+            sb.append("import ").append(importPackage).append(";\n");
+        }
+        return sb.toString();
     }
 
 }
